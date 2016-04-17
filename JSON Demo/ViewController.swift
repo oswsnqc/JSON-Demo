86d1a7f22c9954de6cfc10eb5acc09574b66bf87
Yisen Jing <jyisen@hotmail.com>
2016-04-16 23:47:15 -0700

    JSON study application

--- JSON Demo/ViewController.swift
+++ JSON Demo/ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let urlPath = "http://www.telize.com/geoip"
+        
+        let url = NSURL(string: urlPath)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: {data, error, response -> Void in
+            if error != nil {
+                println(error)
+                
+            } else {
+               let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+                
+               println(jsonResult, "372189379821739812")
+                
+            }
+        })
+        
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
